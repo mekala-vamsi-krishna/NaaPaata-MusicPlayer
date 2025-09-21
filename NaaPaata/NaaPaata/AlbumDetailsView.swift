b2ec91f0c5d352ba3de9939ce24c69ef052ec39c
Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2025-09-21 23:22:12 +0530

    [NEW] Implemented Album Details View

---
+++ NaaPaata/NaaPaata/AlbumDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  AlbumDetailsView.swift
+//  NaaPaata
+//
+//  Created by Mekala Vamsi Krishna on 9/21/25.
+//
+
+import SwiftUI
+
+struct AlbumDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlbumDetailsView()
+}
