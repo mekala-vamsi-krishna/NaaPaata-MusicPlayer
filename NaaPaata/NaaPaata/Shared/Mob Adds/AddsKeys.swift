ff9f32a577341a46b365b0b69023affce2a5ea38
kundancodelab <kundaniosdev@gmail.com>
2026-01-25 19:39:29 +0530

    Repalced ids and unit ids from demo test to real one

--- NaaPaata/NaaPaata/Shared/Mob Adds/AddsKeys.swift
+++ NaaPaata/NaaPaata/Shared/Mob Adds/AddsKeys.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 enum AddsKeys : String {
-    case adAppId = "ca-app-pub-1926766303321524~9539049175"
-    case addUnitId = "ca-app-pub-1926766303321524/7909739906"
+    case adAppId = "ca-app-pub-5349198125883121~3799104506"
+    case addUnitId = "ca-app-pub-5349198125883121/8233173531"
 }
